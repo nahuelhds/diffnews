dad9881c1e48884f27eaee88607e9aa7369c3753
Nahue <nahuelhds@gmail.com>
2023-12-31 16:52:38 -0300

    Extract article (without html processing) and until one day ago

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,40 @@
 import "dotenv/config";
-
-import { fetchFeed } from "./service/feedService.js";
-import { saveToJsonFile } from "./service/fs.js";
 import { FeedConfig } from "./types.js";
 import { feedConfigs } from "./config.js";
 import filenamify from "filenamify";
+import { extract as feedExtractor } from "@extractus/feed-extractor";
+import { extract as articleExtractor } from "@extractus/article-extractor";
+import { saveToJsonFile } from "./utils.js";
+import dayjs from "dayjs";
 
 const STATIC_FOLDER = "./static";
 
-feedConfigs.forEach(async (feedConfig: FeedConfig) => {
-  const feedData = await fetchFeed(feedConfig.url);
-  const destFile = `${STATIC_FOLDER}/${feedConfig.id}.json`;
-  const feedDestFile = saveToJsonFile(destFile, feedData);
-  console.log(`RSS feed saved to ${feedDestFile} successfully!`);
+const fetchOptions = {
+  // signal: AbortSignal.timeout(5000)
+};
 
-  feedData.entries.forEach(entry => {
-    const entryFilename = filenamify(entry.id);
-    const entryDestFile = `${STATIC_FOLDER}/${feedConfig.id}/${entryFilename}.json`;
-    saveToJsonFile(entryDestFile, entry);
-    console.log(`RSS feed saved to ${entryDestFile} successfully!`);
-  });
+const oneDayAgo = dayjs().subtract(1, "day");
+feedConfigs.forEach(async (feedConfig: FeedConfig) => {
+  const feedData = await feedExtractor(feedConfig.url, undefined, fetchOptions);
+  const destFile = `${STATIC_FOLDER}/${feedConfig.id}.json`;
+  saveToJsonFile(destFile, feedData);
+
+  for (const entry of feedData.entries) {
+    try {
+      const published = dayjs(entry.published);
+      if (published < oneDayAgo) {
+        // Don't want to process entries longer that one week
+        return;
+      }
+      const entryData = await articleExtractor(entry.link, undefined, fetchOptions);
+      const entryFilename = filenamify(entry.id);
+      const entryDestFile = `${STATIC_FOLDER}/${feedConfig.id}/${entryFilename}.json`;
+      saveToJsonFile(entryDestFile, entryData);
+      console.log(`ENTRY: ${entry.link}`);
+    } catch (err) {
+      console.warn("Could not parse entry: ", entry.link, err);
+      console.table(entry);
+    }
+  }
 });
 
--- src/service/feedService.ts
+++
@@ -1,9 +0,0 @@
-import { extract as feedExtractor } from "@extractus/feed-extractor";
-
-const feedFetchOptions = {
-  signal: AbortSignal.timeout(5000)
-};
-
-export async function fetchFeed(url: string) {
-  return await feedExtractor(url, undefined, feedFetchOptions);
-}
