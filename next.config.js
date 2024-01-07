505cc4e2af670f577b753d4e589a331dd121deda
Nahue <nahuelhds@gmail.com>
2024-01-07 19:12:03 -0300

    Fix canvas not found cjs

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+/** @type {import("next").NextConfig} */
+const nextConfig = {
+  webpack: (config) => {
+    // FIXME: possible problem with serverless
+    config.externals = [...config.externals, "canvas"];
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
